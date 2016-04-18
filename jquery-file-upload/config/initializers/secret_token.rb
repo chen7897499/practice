ad6e8dbdc1f3e8600e933a1c4f76cdc1532bb909
bomb <chen7897499@gmail.com>
2016-04-18 08:04:22 +0000

    add jquery file upload

---
+++ jquery-file-upload/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Lala::Application.config.secret_token = 'bf2f0364c625598abd3afda9464ea512e8ad1f053480aab9150d338d86c7ce8b71fb460fe3a4308d6d4b070adfcf6bf7464b1fe869e1154011dbbb4718802212'
