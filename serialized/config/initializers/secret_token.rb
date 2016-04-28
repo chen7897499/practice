2945c36d8b543b06cb7c10fa1a2774fed8c55a48
bomb <chen7897499@gmail.com>
2016-04-28 14:54:47 +0000

    serialized-attributes

---
+++ serialized/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Serialized::Application.config.secret_token = 'bd6adb5d48414448a9ff16f55628009bf7b8ed4d36d577c7b0ea0f4642414fd91a8257f4f4d2b56fdb6ead1327392543c1d36a93b11490fa40f52fe6271f73db'
