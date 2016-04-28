08d20f6b29c6837f5fa21cd923af5ac1c183e576
bomb <chen7897499@gmail.com>
2016-04-28 02:15:32 +0000

    ujs ajax

---
+++ ujs_ajax/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Tester::Application.config.secret_token = '7f7850289733e0ae5ca61ce1dae54929cb62a7dc5e4d7f043d330b53c75ac98fffef769c0c79138143d592b5d7aa7f87fcb60b4b348c43d08c52979cc94b15fe'
