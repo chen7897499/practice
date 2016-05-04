80807cda95c0c5f0dd6aad35a13a7038f8b8a391
bomb <chen7897499@gmail.com>
2016-05-04 06:30:17 +0000

    pass hash to hidden_field

---
+++ hashes_hidden_field/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Hashes::Application.config.secret_token = '92bc57489b58ddc51e2be9dfade77e2efe5b069d61798096ae9e89fb0aa41c4f6e5e3103c912167f8062fa008868ae77a308db8b23d14e682e8f0c834ef7fbde'
