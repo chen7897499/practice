892818a179b328ec362e12c730c9d6ca8a66ff69
bomb <chen7897499@gmail.com>
2016-05-09 14:18:29 +0000

    ruby and js sharing data

---
+++ ruby_js_sharing_data/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Pass::Application.config.secret_token = 'b0e004ba952d74f11ea901e6f5298f0eca967e79a312f914e7b040c176d70c2c281fb316fb8ad88e888327f217e6df61903e83c27a0b87da4f9b2df90d2ad0a9'
