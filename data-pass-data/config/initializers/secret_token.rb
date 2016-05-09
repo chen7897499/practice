2d3c46a834da71bd8aaedb2b15718d4e99de52dc
bomb <chen7897499@gmail.com>
2016-05-09 09:13:49 +0000

    sharing data with javascript

---
+++ data-pass-data/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Tables::Application.config.secret_token = '543a3d4c51321a7cc31c3517488fa68b54d3f8dac06af0ce7ad58012812b74f83be97d9af9ee9105ab2c1e8f350ee7b9b948ca12246a99478e79ce5049f69a5d'
