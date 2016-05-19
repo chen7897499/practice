430d9d4e509472a91df86191d4b24eb2ee0db361
bomb <chen7897499@gmail.com>
2016-05-19 14:28:11 +0000

    destroy with ajax

---
+++ destroy_with_ajax/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Delete::Application.config.secret_token = '726ecd5d0397c69ce9c2918e9f98e803413a8cd4721ffde363762c6f106af612250de85545db4ea13471c8c7ddd260e041969b1bfca7438b51c81ad0e0e29eac'
