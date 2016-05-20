a7512f1a89f6a6ebabb7d31f8262fdfd59f2bd56
bomb <chen7897499@gmail.com>
2016-05-20 15:40:49 +0000

    iframe upload

---
+++ iframe_upload/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+AjaxUpload::Application.config.secret_token = '43d77aa04c42ca73a24f890bb8ff85ad8b82879b5e851af990c505bf991560f634755886cefd91857a09a8b60947d5229d8226d70ecd96ecc7494c2e7b489824'
