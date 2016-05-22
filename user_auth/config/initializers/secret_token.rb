e3ecfe78f5c44ea3f25a785d738f7a5b9fcd5359
bomb <chen7897499@gmail.com>
2016-05-22 05:43:41 +0000

    user auth without any gem

---
+++ user_auth/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+UserAuth::Application.config.secret_token = '7f8c981157fe0131c27dbf20297f229dff6790f98fe072153abb0e9ee9b269b277cdbd18b9877e4435cd91ae89ba668fcd4f5787738483688c408d22c5073da0'
