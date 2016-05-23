62101ade2bf80a905fa7a1017ba43f3e1ed80d66
bomb <chen7897499@gmail.com>
2016-05-23 14:19:16 +0000

    client side validation

---
+++ client_side_validation/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+ClientSideValidation::Application.config.secret_token = '3d05239097fb605e0bdc4c3c1ae1abb7198dcd3f0d59ba1ff817075c93cecd920689677f091070e0fdc23f82f9699829859bf639f28eb14a5d2ab27e5b56b424'
