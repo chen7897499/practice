8b4fee24509310c526d33c0c3a64bba01000471a
bomb <chen7897499@gmail.com>
2016-05-23 12:50:41 +0000

    in place edit

---
+++ in_place_edit/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+InPlaceEdit::Application.config.secret_token = '64f507129ee22a7a3a9ce80b126235babc8568e09803e1f77228bf5093a36be6b184e3f65f77b602b5a8c4a79115bb8174345bf6ba86b48ef4d96bd9ebaf9ea4'
